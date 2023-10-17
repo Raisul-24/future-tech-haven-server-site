042b3d7e1047200a98c0133e83dee7cdf20d7922
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-10-18 00:10:41 +0600

    user data upload to database

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 50001
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sxdrhxr.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+      const userCollection = client.db('futureTechHavenDB').collection('user');
+      app.post('/user', async (req, res) => {
+         const user = req.body;
+         console.log(user);
+         const result = await userCollection.insertOne(user)
+         res.send(result)
+      });
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      //  await client.close();
+   }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+   res.send("All Ok!!")
+})
+
+app.listen(port, () => {
+   console.log(`Server is running on port: ${port}`)
+})
