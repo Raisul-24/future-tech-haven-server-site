63d755a3059a1ea174f826683c0bd30847080e2b
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-10-19 15:32:25 +0600

    add update products and added to my cart to db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 50001
@@ -25,20 +25,56 @@ async function run() {
 
       const userCollection = client.db('futureTechHavenDB').collection('user');
       const productCollection = client.db('futureTechHavenDB').collection('product');
+      const addCartsCollection = client.db('futureTechHavenDB').collection('myCart');
 
       // product
       app.get('/products',async(req,res) =>{
          const cursor = productCollection.find();
          const result = await cursor.toArray();
          res.send(result);
-      })
-
+      });
+// product id
+      app.get('/products/:id',async(req,res) =>{
+         const id = req.params.id;
+         const query = {_id : new ObjectId(id)}
+         const result = await productCollection.findOne(query);
+         res.send(result);
+      });
+//  add products
       app.post('/products',async(req,res) =>{
          const product = req.body;
          console.log(product);
          const result = await productCollection.insertOne(product);
          res.send(result);
       });
+      // add to card products
+      app.post('/addToCarts',async(req,res) =>{
+         const cartProduct = req.body;
+         console.log(cartProduct);
+         const result = await addCartsCollection.insertOne(cartProduct);
+         res.send(result);
+      });
+   // update product
+   app.put('/products/:id',async(req,res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const options = {upsert : true}
+      const updatedProduct = {
+         $set: {
+            // name,brand,type,price,short_description,rating,photo,details
+            name:req.body.name, 
+            brand:req.body.brand, 
+            type:req.body.type, 
+            price:req.body.price, 
+            short_description:req.body.short_description, 
+            rating:req.body.rating, 
+            photo:req.body.photo, 
+            details:req.body.details, 
+         }
+      }
+      const result = await productCollection.updateOne(filter, updatedProduct, options);
+      res.send(result)
+    })
 
       // user
       app.post('/user', async (req, res) => {
